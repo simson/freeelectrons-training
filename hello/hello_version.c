cef54a66f47f8d519037c9e875e49c60e4d86a3c
Siméon Marijon <simeon.marijon@kalray.eu>
2015-07-21 14:36:20 +0200

    hello: add time manipulation for getting insertion time of the hello module

--- hello/hello_version.c
+++ hello/hello_version.c
@@ -7,12 +7,14 @@ static char* message  = "TEST";
 module_param(message, charp, S_IRUGO| S_IWUSR);
 static int howmany = 1;
 module_param(howmany, int, S_IRUGO| S_IWUSR);
+struct timeval tv_init, tv_exit, tv_res  = { 0 } ;
 
 static int __init hello_init(void)
 {
 	int i;
 	struct new_utsname * name;
 	name =  utsname();
+	do_gettimeofday(&tv_init);
 	pr_alert("Version sysname %s, \
 		 nodename %s, \
 		 release %s, \
@@ -33,10 +35,33 @@ static int __init hello_init(void)
 	return 0;
 }
 
+static void timersub(const struct timeval* tvp, const struct timeval* uvp,
+		     struct timeval* vvp)
+{
+	vvp->tv_sec = tvp->tv_sec - uvp->tv_sec;
+	vvp->tv_usec = tvp->tv_usec - uvp->tv_usec;
+	if (vvp->tv_usec < 0)
+	{
+		--vvp->tv_sec;
+		vvp->tv_usec += 1000000;
+	}
+}
 static void __exit hello_exit(void)
 {
+	int days, hours, minutes, seconds, microseconds;
+	do_gettimeofday(&tv_exit);
+	timersub(&tv_exit, &tv_init, &tv_res);
+	days = tv_res.tv_sec / 86400; // Integer division rounds down.
+	hours = (tv_res.tv_sec % 86400) / 3600;
+	minutes = (tv_res.tv_sec % 3600) / 60;
+	seconds = (tv_res.tv_sec % 60);
+	microseconds = (tv_res.tv_usec);
 	pr_alert("FIN DU TEST : mon premier module");
+	pr_alert("Il a vécu %d jours %d heure %d minutes %d secondes et %d \
+		 microsecondes\n", days, hours, minutes, seconds, microseconds);
 }
+
+
 
 module_init(hello_init);
 module_exit(hello_exit);
