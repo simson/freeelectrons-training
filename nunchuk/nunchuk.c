339171196bf4ed0535be09b72418498a1b88a9d2
Siméon Marijon <simeon.marijon@kalray.eu>
2015-07-22 16:54:59 +0200

    nunchuk : Probe will never end and show periodically the state off the nunchuk based on i2c reading

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -1,4 +1,5 @@
 #include <linux/init.h>
+#include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/slab.h>
@@ -22,17 +23,95 @@ MODULE_DEVICE_TABLE(of, nunchuk_dt_ids);
 static int last_idx = 0;
 struct nunchuk_info {
 	int idx;
+	char x_pos;
+	char y_pos;
+	short acc_x;
+	short acc_y;
+	short acc_z;
+	char c_pressed;
+	char z_pressed;
 };
+
+static int handshake(struct i2c_client *client)
+{
+	int ret;
+	char uncrypted_msg[] = { 0xf0 , 0x55 };
+	char init_msg[] = { 0xfb , 0x00 };
+		/* Send the uncrypted communication message */
+	ret = i2c_master_send(client, uncrypted_msg, 2);
+	pr_alert("I2c transfer returned %d\n", ret);
+	if (ret != 1) {
+		pr_alert("I2c transfer failed\n");
+	}
+	udelay(1000);
+	/*Complete the init */
+	ret = i2c_master_send(client, init_msg, 2);
+	pr_alert("I2c transfer returned %d\n", ret);
+	if (ret != 1) {
+		pr_alert("I2c transfer failed\n");
+	}
+	return 0;
+}
+
+static int nunchuk_read_registers(struct i2c_client *client)
+{
+	char read_msg[] = { 0x00 };
+	char state[6] = { 0x00 };
+	int ret;
+	mdelay(10);
+	ret = i2c_master_send(client, read_msg, 1);
+	pr_alert("I2c transfer returned %d\n", ret);
+	if (ret != 1) {
+		pr_alert("I2c transfer failed\n");
+		return -EIO;
+	}
+	mdelay(10);
+	ret = i2c_master_recv(client, state, 6);
+	pr_alert("I2c transfer returned %d\n", ret);
+	if (ret != 6) {
+		pr_alert("I2c transfer failed\n");
+		return -EIO;
+	}
+	struct nunchuk_info* pdata = i2c_get_clientdata(client);
+	pdata->x_pos = state[0] ;
+	pdata->y_pos = state[1];
+	pdata->acc_x = (state[2] << 2) | ( ( state[5] & 0x0C ) >> 2 );
+	pdata->acc_y = (state[3] << 2) | ( ( state[5] & 0x30 ) >> 4 );
+	pdata->acc_z = (state[4] << 2) | ( ( state[5] & 0xC0 ) >> 6 );
+	pdata->c_pressed = ! ( (state[5] & 0x2 ) >> 1);
+	pdata->z_pressed = ! (state[5] & 0x1 );
+	return 0;
+}
+
+static int nunchuk_display_state(struct nunchuk_info *pdata)
+{
+	pr_alert("X = %hhu\nY = %hhu\nAcc_X = %hu\nAcc_Y = %hu\nAcc_Z = %hu\nC = %hhu\nZ = %hhu\n",
+	 pdata->x_pos,
+	 pdata->y_pos,
+	 pdata->acc_x,
+	 pdata->acc_y,
+	 pdata->acc_z,
+	 pdata->c_pressed,
+	 pdata->z_pressed);
+}
 
 static int nunchuk_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
 	/* initialize device */
+	handshake(client);
 	struct nunchuk_info* pdata = kmalloc(sizeof(struct nunchuk_info), GFP_KERNEL);
 	pdata->idx = ++last_idx;
 	/* register to a kernel framework */
 	i2c_set_clientdata(client, pdata);
 	pr_alert("Nunchuk detected id : %d/%d\n",pdata->idx,last_idx);
+	while(1){
+		nunchuk_read_registers(client);
+		nunchuk_read_registers(client);
+		nunchuk_display_state(pdata);
+		mdelay(1000);
+	}
+
 	return 0;
 }
 
